0e72067940aac5726b7d1785115ab08061842cff
V. Jackson <vjackson725@users.noreply.github.com>
2023-06-07 15:04:57 +1000

    seplogic: show trace semantics forms a semiring + other algebras

--- Semantics.thy
+++ Semantics.thy
@@ -3,7 +3,7 @@ theory Semantics
 begin
 
 notation(input)
-  times (infixl \<open>\<parallel>\<close> 65)
+  times (infixl \<open>\<parallel>\<close> 70)
 
 text \<open> A process can be represented as a trace of predicate pairs. \<close>
 
@@ -17,10 +17,26 @@ datatype (\<alpha>set: 'a) trace =
     map: trace_map
     pred: trace_all    
     rel: trace_all2
-    
 
 lemma not_TNil_eq: \<open>a \<noteq> TNil \<longleftrightarrow> (\<exists>ax a'. a = ax \<cdot> a')\<close>
   using trace.exhaust_sel by blast
+
+subsection \<open> Trace Concat \<close>
+find_theorems \<open>_ @ _\<close>
+
+fun trace_concat :: \<open>'a trace \<Rightarrow> 'a trace \<Rightarrow> 'a trace\<close> (infixr \<open>@t\<close> 65) where
+  \<open>trace_concat TNil b = b\<close>
+| \<open>trace_concat (x \<cdot> a) b = x \<cdot> trace_concat a b\<close>
+
+lemma trace_concat_unit_right[simp]:
+  \<open>a @t TNil = a\<close>
+  by (induct a) simp+
+
+lemma trace_concat_assoc:
+  \<open>a @t b @t c = (a @t b) @t c\<close>
+  by (induct a arbitrary: b c) simp+
+
+subsection \<open> Parallel Trace Merge\<close>
 
 inductive merge_traces_rel :: \<open>'a trace \<Rightarrow> 'a trace \<Rightarrow> 'a trace \<Rightarrow> bool\<close> where
   mergetr_nil: \<open>merge_traces_rel TNil TNil TNil\<close>
@@ -184,11 +200,6 @@ lemma merge_traces_rel_assoc:
 definition merge_traces :: \<open>'a trace \<Rightarrow> 'a trace \<Rightarrow> 'a trace set\<close> where
   \<open>merge_traces a b \<equiv> Collect (merge_traces_rel a b)\<close>
 
-fun tmp :: \<open>'a trace \<Rightarrow> 'a trace \<Rightarrow> 'a trace set\<close> where
-  \<open>tmp TNil b = {b}\<close>
-| \<open>tmp a TNil = {a}\<close>
-| \<open>tmp (x \<cdot> a) (y \<cdot> b) = ((TCons x ` tmp a (y \<cdot> b)) \<union> (TCons y ` tmp (x \<cdot> a) b))\<close>
-
 lemma merge_traces_simp[simp]:
   \<open>merge_traces TNil b = {b}\<close>
   \<open>merge_traces a TNil = {a}\<close>
@@ -243,14 +254,49 @@ lemma merge_traces_assoc:
         a \<in> A \<and> b \<in> B \<and> c \<in> C \<and> bc \<in> merge_traces b c \<and> abc \<in> merge_traces a bc}\<close>
   by (simp add: merge_traces_def merge_traces_rel_assoc)
 
+lemma merge_traces_rel_comm:
+  \<open>merge_traces_rel a b c = merge_traces_rel b a c\<close>
+  by (induct c arbitrary: a b)
+    (force simp add: merge_traces_rel_cons_out_iff)+
+
+lemma merge_traces_comm:
+  \<open>merge_traces a b = merge_traces b a\<close>
+  by (simp add: merge_traces_def set_eq_iff merge_traces_rel_comm)
+
+lemma merge_traces_not_empty:
+  \<open>merge_traces a b \<noteq> {}\<close>
+  by (induct a b rule: merge_traces_induct)
+     (force simp add: merge_traces_def merge_traces_rel_cons_LR_iff)+
+
 section \<open>Processes\<close>
 
 type_synonym 'a ptrace = \<open>('a pred \<times> 'a pred) trace\<close>
 
-datatype 'a process = Process (proctr: \<open>'a ptrace set\<close>)
+datatype 'a process = Process (proctr: \<open>'a trace set\<close>)
 
-instantiation process :: (type) monoid_mult
+lemma process_eq_iff:
+  \<open>a = b \<longleftrightarrow> proctr a = proctr b\<close>
+  using process.expand by auto
+
+
+lemma eq_Process_iff_proctr_eq:
+  \<open>a = Process A \<longleftrightarrow> proctr a = A\<close>
+  by auto
+
+lemma Process_eq_iff_eq_proctr:
+  \<open>Process A = a \<longleftrightarrow> A = proctr a\<close>
+  by auto
+
+subsection \<open> Processes are a semiring \<close>
+
+instantiation process :: (type) comm_semiring_1
 begin
+
+definition plus_process :: \<open>'a process \<Rightarrow> 'a process \<Rightarrow> 'a process\<close> where
+  \<open>plus_process A B \<equiv> Process (proctr A \<union> proctr B)\<close>
+
+definition zero_process :: \<open>'a process\<close> where
+  \<open>zero_process \<equiv> Process {}\<close>
 
 definition times_process :: \<open>'a process \<Rightarrow> 'a process \<Rightarrow> 'a process\<close> where
   \<open>times_process A B \<equiv> Process (\<Union>(a,b) \<in> (proctr A \<times> proctr B). merge_traces a b)\<close>
@@ -261,19 +307,112 @@ definition one_process :: \<open>'a process\<close> where
 instance
 proof
   fix a b c :: \<open>'a process\<close>
+  show \<open>a + b + c = a + (b + c)\<close>
+    by (force simp add: plus_process_def)
+  show \<open>0 + a = a\<close>
+    by (simp add: plus_process_def zero_process_def)
+  show \<open>a + b = b + a\<close>
+    by (simp add: plus_process_def sup_commute)
   show \<open>a \<parallel> b \<parallel> c = a \<parallel> (b \<parallel> c)\<close>
-    apply (simp add: times_process_def one_process_def Setcompr_eq_image Union_eq)
+    apply (simp add: times_process_def Setcompr_eq_image Union_eq)
     apply (rule HOL.trans[OF _ merge_traces_assoc, THEN HOL.trans])
      apply blast
     apply blast
     done
   show \<open>1 \<parallel> a = a\<close>
     by (simp add: times_process_def one_process_def Setcompr_eq_image)
-  show \<open>a \<parallel> 1 = a\<close>
-    by (simp add: times_process_def one_process_def Setcompr_eq_image)
+  show \<open>a \<parallel> b = b \<parallel> a\<close>
+    using merge_traces_comm
+    by (fastforce simp add: times_process_def set_eq_iff)
+  show \<open>0 \<parallel> a = 0\<close>
+    by (simp add: zero_process_def times_process_def)
+  show \<open>a \<parallel> 0 = 0\<close>
+    by (simp add: zero_process_def times_process_def)
+  show \<open>(a + b) \<parallel> c = a \<parallel> c + b \<parallel> c\<close>
+    by (simp add: plus_process_def times_process_def Sigma_Un_distrib1)
+  show \<open>(0::'a process) \<noteq> 1\<close>
+    by (simp add: zero_process_def one_process_def)
 qed
 
 end
 
+instantiation process :: (type) monoid_seq
+begin
+
+definition seq_process :: \<open>'a process \<Rightarrow> 'a process \<Rightarrow> 'a process\<close> where
+  \<open>seq_process A B \<equiv> Process {a @t b|a b. a \<in> proctr A \<and> b \<in> proctr B}\<close>
+
+definition skip_process :: \<open>'a process\<close> where
+  \<open>skip_process \<equiv> Process {TNil}\<close>
+
+lemma process_skip_eq_one:
+  \<open>(\<I>::'a process) = 1\<close>
+  by (simp add: one_process_def skip_process_def)
+
+instance
+  apply standard
+    apply (simp add: seq_process_def set_eq_iff, metis trace_concat_assoc)
+   apply (simp add: seq_process_def skip_process_def)
+  apply (simp add: seq_process_def skip_process_def)
+  done
+
+end
+
+text \<open>TODO: should probably be a new class\<close>
+lemma seq_zero_zero[simp]:
+  fixes a :: \<open>'a process\<close>
+  shows
+    \<open>a \<triangleright> 0 = 0\<close>
+    \<open>0 \<triangleright> a = 0\<close>
+  by (simp add: seq_process_def zero_process_def)+
+
+lemma seq_plus_eq:
+  fixes a :: \<open>'a process\<close>
+  shows
+    \<open>a \<triangleright> (b + c) = (a \<triangleright> b) + (a \<triangleright> c)\<close>
+    \<open>(a + b) \<triangleright> c = (a \<triangleright> c) + (b \<triangleright> c)\<close>
+  by (force simp add: seq_process_def plus_process_def)+
+
+instance process :: (type) semiring_no_zero_divisors
+  by standard
+    (simp add: times_process_def zero_process_def eq_Process_iff_proctr_eq merge_traces_not_empty)
+
+instance process :: (type) semiring_1_no_zero_divisors
+  by standard
+
+instantiation process :: (type) bounded_semilattice_inf_top
+begin
+
+definition top_process :: \<open>'a process\<close> where
+  \<open>top_process \<equiv> Process {}\<close>
+
+lemma process_top_eq_zero: \<open>(\<top>::'a process) = 0\<close>
+  by (simp add: top_process_def zero_process_def)
+
+definition inf_process :: \<open>'a process \<Rightarrow> 'a process \<Rightarrow> 'a process\<close> where
+  \<open>inf_process a b \<equiv> Process (proctr a \<union> proctr b)\<close>
+
+lemma process_inf_eq_plus: \<open>(a::'a process) \<sqinter> b = a + b\<close>
+  by (simp add: inf_process_def plus_process_def)
+
+definition less_eq_process :: \<open>'a process \<Rightarrow> 'a process \<Rightarrow> bool\<close> where
+  \<open>less_eq_process a b \<equiv> proctr a \<supseteq> proctr b\<close>
+
+definition less_process :: \<open>'a process \<Rightarrow> 'a process \<Rightarrow> bool\<close> where
+  \<open>less_process a b \<equiv> proctr a \<supset> proctr b\<close>
+
+instance
+  by standard
+    (force simp add: less_process_def less_eq_process_def inf_process_def top_process_def
+      process_eq_iff)+
+
+end
+
+instance process :: (type) ordered_semiring_0
+  apply standard
+    apply (force simp add: less_eq_process_def plus_process_def)
+   apply (simp add: less_eq_process_def zero_process_def times_process_def)
+  apply (simp add: less_eq_process_def zero_process_def times_process_def)
+  done
 
 end
